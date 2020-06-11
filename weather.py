ee40edb50fa0e442a8e4e8a600387dff73237276
Gaoyagi <aoyagigeorge@gmail.com>
2020-06-11 00:13:25 -0700

    first commit

---
+++ weather.py
@@ -0,0 +1,9 @@
+import requests
+
+
+weather = requests.get("api.openweathermap.org/data/2.5/weather?zip=94108,us&appid=48b7a5079c5920a0bb7a4b60dde5b970")
+weather = weather.json()
+
+print("The current weather at Nob Hill is", weather["weather"][0]["description"])
+mood = input("What is your current mood: ")
+print("thanks for responding :)")
